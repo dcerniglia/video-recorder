a3a5789e1f2ed5136c715da061f3c7617bd2cbd3
David Cerniglia <dcerniglia@thedevshop.com>
2023-03-11 13:58:15 -0500

    video recorder working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, OnDestroy, PLATFORM_ID, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'video-recorder';
+  @ViewChild('video', {static: true}) video: ElementRef<HTMLVideoElement>;
+
+
+  constructor(@Inject(PLATFORM_ID) private _platform: Object) {}
+
+  onStart(){
+    if(isPlatformBrowser(this._platform) && 'mediaDevices' in navigator) {
+      navigator.mediaDevices.getUserMedia({video: true}).then((ms: MediaStream) => {
+        const _video = this.video.nativeElement;
+        _video.srcObject = ms;
+        _video.play(); 
+      });
+    }
+  }
+
+  onStop() {
+    this.video.nativeElement.pause();
+    (this.video.nativeElement.srcObject as MediaStream).getVideoTracks()[0].stop();
+    this.video.nativeElement.srcObject = null;
+  }
+
+  ngOnDestroy() {
+    (this.video.nativeElement.srcObject as MediaStream).getVideoTracks()[0].stop();
+  }
 }
